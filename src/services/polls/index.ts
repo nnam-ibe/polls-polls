2e97ee14a0729006aefb569ba81f2a05942c3c38
Nnamdi Ibe <nnam.ibe@gmail.com>
2023-10-01 00:39:06 -0400

    moved poll functions

--- src/services/polls/index.ts
+++ src/services/polls/index.ts
@@ -6,7 +6,12 @@ import {
   dbSingleVotes,
 } from "@/db/schema";
 import { AppError } from "@/lib/error-handler";
-import { eq } from "drizzle-orm";
+import type { PollQuery } from "@/lib/types";
+import { and, eq, isNull } from "drizzle-orm";
+import {
+  calculateRankedResult,
+  calculateSingleResult,
+} from "@/services/polls/collate-votes";
 
 export async function getPoll(id: string) {
   const poll = await db.query.dbPolls.findFirst({
@@ -22,6 +27,39 @@ export async function getPoll(id: string) {
     throw new AppError("Poll not found", 400);
   }
   return poll;
+}
+
+export async function getPolls(parameters: PollQuery) {
+  let whereClause = and(eq(dbPolls.isActive, true), isNull(dbPolls.deletedAt));
+  if ("isClosed" in parameters && parameters.isClosed !== undefined) {
+    whereClause = and(whereClause, eq(dbPolls.isClosed, parameters.isClosed));
+  }
+  const polls = await db.query.dbPolls.findMany({
+    where: whereClause,
+  });
+  return polls;
+}
+
+export async function getPollResult(id: string) {
+  const poll = await db.query.dbPolls.findFirst({
+    with: {
+      PollChoices: true,
+      SingleVotes: true,
+      RankedVotes: true,
+    },
+    where: eq(dbPolls.id, id),
+  });
+
+  if (!poll) {
+    throw new AppError("Poll not found", 400);
+  }
+
+  const result =
+    poll.voteType === "ranked"
+      ? calculateRankedResult(poll)
+      : calculateSingleResult(poll);
+
+  return { poll, result };
 }
 
 export async function deletePoll(id: string, userId: string | null) {
